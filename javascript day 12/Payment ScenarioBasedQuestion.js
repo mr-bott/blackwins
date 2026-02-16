0cbf6bfbad5972403186b0ce0b2231237c550d49
Murali Krishna <muralikrishna8309@gmail.com>
2026-02-16 13:52:52 +0530

    solved Scenario based question

---
+++ javascript day 12/Payment ScenarioBasedQuestion.js
@@ -0,0 +1,32 @@
+function fetchPayment(){
+    return new Promise((resolve,reject)=>{
+       setTimeout(()=>{
+        resolve("payment done");
+       },1500);
+    });
+}
+
+function fetchTimeout(){
+    return new Promise((resolve,reject)=>{
+       setTimeout(()=>{
+        reject("Timeout");
+       },1000);
+    });
+}
+
+
+async function processPayment(){
+  try{
+     const result=await Promise.race([
+        fetchPayment(),
+        fetchTimeout()
+     ]);
+
+     console.log(result);
+  }
+  catch(e){
+    console.log("Error while paying",e);
+  }
+}
+
+processPayment();
